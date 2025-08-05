d44d1a28d38bcb0be800c6fd168130f79874a735
KrLite <wang070621@foxmail.com>
2025-08-06 01:26:12 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -22,8 +22,10 @@ const MAX_RETRY: u8 = 5;
 
 #[tokio::main]
 async fn main() {
-    dotenvy::dotenv().unwrap();
+    dotenvy::dotenv().ok();
     logging::setup().unwrap();
+
+    info!("Loaded environment: {:#?}", std::env::vars());
     info!("Starting server on port {PORT}");
 
     let mut app = Router::new();
