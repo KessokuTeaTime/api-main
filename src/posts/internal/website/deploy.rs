820acdc97f87920ce1130ed59c701e5c74432a8b
KrLite <wang070621@foxmail.com>
2025-08-05 16:16:57 +0800

    🚚 Rename some fields

--- src/posts/internal/website/deploy.rs
+++ src/posts/internal/website/deploy.rs
@@ -18,23 +18,23 @@ use std::{fs, io, thread};
 use tokio_util::io::StreamReader;
 
 /// The pending [`WorkflowInfo`] to be deployed.
-static PENDING_INFO: LazyLock<Mutex<Option<WorkflowInfo>>> = LazyLock::new(|| Mutex::new(None));
+static PENDING_INFO: LazyLock<Mutex<Option<Body>>> = LazyLock::new(|| Mutex::new(None));
 /// Indicates whether a worker thread is currently running.
 static IS_THREAD_RUNNING: AtomicBool = AtomicBool::new(false);
 
 #[derive(Debug, Deserialize, Clone)]
-pub struct WorkflowInfo {
+pub struct Body {
     pub run_id: String,
     pub dest: String,
 }
 
-impl Display for WorkflowInfo {
+impl Display for Body {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_fmt(format_args!("{}<~{}", self.dest, self.run_id))
     }
 }
 
-impl WorkflowInfo {
+impl Body {
     pub fn validate(self) -> Self {
         Self {
             run_id: self.run_id,
@@ -48,7 +48,7 @@ impl WorkflowInfo {
 
 /// Responds to a website deployment request.
 #[post("/internal/website/deploy")]
-async fn post(req: HttpRequest, info: web::Json<WorkflowInfo>) -> impl Responder {
+async fn post(req: HttpRequest, body: web::Json<Body>) -> impl Responder {
     debug!(
         "Received notification from {}, authenticating…",
         req.connection_info()
@@ -85,19 +85,19 @@ async fn post(req: HttpRequest, info: web::Json<WorkflowInfo>) -> impl Responder
         Ok(_) => {
             // TODO: worker threads may no longer be required as we are using async to fetch res for now
             // Spawns a new worker thread
-            debug!("Spawning a thread with {info:?}");
-            thread::spawn(move || deploy(info.clone().validate()));
+            debug!("Spawning a thread with {body:?}");
+            thread::spawn(move || deploy(body.clone().validate()));
         }
         Err(_) => {
-            // Suspends the latest info
+            // Suspends the latest request
             let mut guard = PENDING_INFO.lock();
-            match guard.replace(info.clone()) {
+            match guard.replace(body.clone()) {
                 None => {
-                    warn!("A thread is already running! Suspending deployment with {info:?}");
+                    warn!("A thread is already running! Suspending deployment with {body:?}");
                 }
-                Some(old_info) => {
+                Some(old_body) => {
                     warn!(
-                        "A thread is already running! Suspending deployment with {info:?}, replacing {old_info:?}"
+                        "A thread is already running! Suspending deployment with {body:?}, replacing {old_body:?}"
                     );
                 }
             }
@@ -124,21 +124,21 @@ where
 }
 
 /// Deploys the website with a [`WorkflowInfo`].
-async fn deploy(info: WorkflowInfo) {
-    let mut info = info;
+async fn deploy(body: Body) {
+    let mut body = body;
 
     'worker_loop: loop {
         let mut retry: u8 = 0;
 
         'artifact_loop: loop {
             // Fetches the artifact
-            let artifact = match fetch_artifact("KessokuTeaTime", "website", &info.run_id).await {
+            let artifact = match fetch_artifact("KessokuTeaTime", "website", &body.run_id).await {
                 State::Success(artifact) => {
-                    info!("Fetched artifact with {info:?}");
+                    info!("Fetched artifact with {body:?}");
                     artifact
                 }
                 State::Retry => {
-                    error!("Failed to fetch artifact with {info:?}");
+                    error!("Failed to fetch artifact with {body:?}");
                     match retry_if_possible(&mut retry) {
                         Ok(_) => continue 'artifact_loop,
                         Err(_) => break 'artifact_loop,
@@ -150,11 +150,11 @@ async fn deploy(info: WorkflowInfo) {
             // Downloads the artifact
             let stream = match download_artifact(artifact).await {
                 State::Success(stream) => {
-                    info!("Downloaded artifact with {info:?}");
+                    info!("Downloaded artifact with {body:?}");
                     stream
                 }
                 State::Retry => {
-                    error!("Failed to download artifact with {info:?}");
+                    error!("Failed to download artifact with {body:?}");
                     match retry_if_possible(&mut retry) {
                         Ok(_) => continue 'artifact_loop,
                         Err(_) => break 'artifact_loop,
@@ -174,15 +174,15 @@ async fn deploy(info: WorkflowInfo) {
             let zip_reader = ZipFileReader::with_tokio(StreamReader::new(
                 stream.map_err(|err| io::Error::new(io::ErrorKind::Other, err)),
             ));
-            match extract_archive(&mut archive, &info.dest) {
+            match extract_archive(&mut archive, &body.dest) {
                 Ok(_) => {
                     info!(
                         "Successfully deployed to {} with {}!",
-                        info.dest, info.run_id
+                        body.dest, body.run_id
                     )
                 }
                 Err(err) => {
-                    error!("Failed to extract destination archive with {info:?}: {err}");
+                    error!("Failed to extract destination archive with {body:?}: {err}");
                 }
             }
 
@@ -193,9 +193,9 @@ async fn deploy(info: WorkflowInfo) {
             let mut guard = PENDING_INFO.lock();
             match guard.take() {
                 None => break 'worker_loop,
-                Some(pending_info) => {
-                    info!("Resolving pending deployment: {pending_info:?}");
-                    info = pending_info;
+                Some(pending_body) => {
+                    info!("Resolving pending deployment: {pending_body:?}");
+                    body = pending_body;
                 }
             }
         }
