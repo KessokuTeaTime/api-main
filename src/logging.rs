7d4385ec4de694320bf36c39a3b537dfb2afe1a9
KrLite <wang070621@foxmail.com>
2025-08-09 14:51:43 +0800

    ✨ Add `latest.log`

--- src/logging.rs
+++ src/logging.rs
@@ -9,7 +9,7 @@ use tracing_subscriber::{
 
 use crate::env::{TRACING_DIR, TRACING_MAX_FILES};
 
-/// Setup the logging component, which contains a stderr layer and a rolling file layer.
+/// Setup the logging component, which contains a stderr layer, a rolling file layer, and a latest file layer.
 ///
 /// # Errors
 ///
@@ -20,11 +20,21 @@ pub fn setup() -> Result<(), Error> {
     let stderr_layer = tracing_subscriber::fmt::layer()
         .pretty()
         .with_writer(std::io::stdout);
+
     let rolling_file_layer = tracing_subscriber::fmt::layer().with_writer(
         RollingFileAppender::builder()
             .filename_suffix("log")
             .rotation(Rotation::DAILY)
             .max_log_files(*TRACING_MAX_FILES)
+            .build(&*TRACING_DIR)?,
+    );
+
+    let latest_file_layer = tracing_subscriber::fmt::layer().with_writer(
+        RollingFileAppender::builder()
+            .filename_prefix("latest")
+            .filename_suffix("log")
+            .rotation(Rotation::DAILY)
+            .max_log_files(1)
             .build(&*TRACING_DIR)?,
     );
 
@@ -36,6 +46,12 @@ pub fn setup() -> Result<(), Error> {
         )
         .with(
             rolling_file_layer
+                .with_timer(ChronoLocal::rfc_3339())
+                .with_ansi(false)
+                .with_filter(LevelFilter::TRACE),
+        )
+        .with(
+            latest_file_layer
                 .with_timer(ChronoLocal::rfc_3339())
                 .with_ansi(false)
                 .with_filter(LevelFilter::TRACE),
