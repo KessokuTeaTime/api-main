c2ee8799d08f582f1707131bb98f94a67418351d
KrLite <wang070621@foxmail.com>
2026-01-05 16:50:43 +0800

    feat: update api-framework

--- src/env.rs
+++ src/env.rs
@@ -19,36 +19,36 @@ pub mod info {
 }
 
 static_lazy_lock! {
+    /// The port to listen to.
     pub PORT: u16 = parse_env!("PORT" => |s| s.parse::<u16>(); anyhow).expect("PORT not set in environment");
-    "The port to listen to."
 }
 
 static_lazy_lock! {
+    /// The username of the API key.
     pub KTT_API_USERNAME: String = env::var("KTT_API_USERNAME").expect("KTT_API_USERNAME not set in environment");
-    "The username of the API key."
 }
 
 static_lazy_lock! {
+    /// The password of the API key.
     pub KTT_API_PASSWORD: String = env::var("KTT_API_PASSWORD").expect("KTT_API_PASSWORD not set in environment");
-    "The password of the API key."
 }
 
 static_lazy_lock! {
+    /// The maximum file count to use for tracing.
     pub TRACING_MAX_FILES: usize = parse_env!("TRACING_MAX_FILES" => |s| s.parse::<usize>(); anyhow).unwrap_or(5);
-    "The maximum file count to use for tracing."
 }
 
 static_lazy_lock! {
+    /// The directory for tracing files. Defaults to `/tmp/tracing/main` if not specified.
     pub TRACING_DIR: PathBuf = parse_env!("TRACING_DIR" => |s| Ok(PathBuf::from(s))).unwrap_or(PathBuf::from("/tmp/tracing")).join(clap::crate_name!());
-    "The directory for tracing files. Defaults to `/tmp/tracing/main` if not specified."
 }
 
 static_lazy_lock! {
+    /// The name of the Docker container that runs this service.
     pub DOCKER_CONTAINER_NAME: String = env::var("DOCKER_CONTAINER_NAME").unwrap_or_else(|_| format!("api-{}", clap::crate_name!()));
-    "The name of the Docker container that runs this service."
 }
 
 static_lazy_lock! {
+    /// The path to the Docker Compose file.
     pub DOCKER_COMPOSE_FILE: PathBuf = parse_env!("DOCKER_COMPOSE_FILE" => |s| Ok(PathBuf::from(s))).unwrap();
-    "The path to the Docker Compose file."
 }
--- src/middleware/auth.rs
+++ src/middleware/auth.rs
@@ -8,11 +8,9 @@ pub mod layers {
     use tower_http::auth::AddAuthorizationLayer;
 
     static_lazy_lock! {
+        /// The layer that authorizes requests with the KessokuTeaTime private CI key in Base 64 format.
+        ///
+        /// See: [`KTT_API_USERNAME`], [`KTT_API_PASSWORD`], [`AddAuthorizationLayer`]
         pub KESSOKU_PRIVATE_CI_AUTHORIZATION: AddAuthorizationLayer = AddAuthorizationLayer::basic(&KTT_API_USERNAME, &KTT_API_PASSWORD);
-        r#"
-        The layer that authorizes requests with the KessokuTeaTime private CI key in Base 64 format.
-
-        See: [`KTT_API_USERNAME`], [`KTT_API_PASSWORD`], [`AddAuthorizationLayer`]
-        "#
     }
 }
